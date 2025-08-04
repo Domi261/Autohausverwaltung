b7e02565f27e0d1908da5361cb7c1279a13f23c6
Domi261 <dominick.schustereit@outlook.de>
2025-08-04 21:17:22 +0200

    Preise angepasst & Auto hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ import service.FahrzeugVerwaltung;
 
 public class Main {
     public static void main(String[] args) {
-        Fahrzeug auto = new Fahrzeug(1, 2020, 10000, "BMW", "X5", 30000, FahrzeugStatus.VERFUEGBAR);
-
+        Fahrzeug auto1 = new Fahrzeug(1, 2005, 100000, "Mercedes Benz", "CLS 500 AMG", 25000, FahrzeugStatus.VERFUEGBAR);
+        Fahrzeug auto2 = new Fahrzeug(2, 2020, 10000, "BMW", "M5 Competition", 90000, FahrzeugStatus.IM_SERVICE);
         FahrzeugVerwaltung verwaltung = new FahrzeugVerwaltung();
-        verwaltung.fahrzeugHinzufuegen(auto);
+
+        verwaltung.fahrzeugHinzufuegen(auto1);
+        verwaltung.fahrzeugHinzufuegen(auto2);
 
         verwaltung.alleFahrzeugeAnzeigen();
     }
+
 }
